b620cd580a71af3a30b39ee91cc80b085b62f170
Max Winslow <maxtwinslow1@gmail.com>
2021-05-18 19:21:23 -0600

    Add cards

--- src/components/GamePlayComponent.vue
+++ src/components/GamePlayComponent.vue
@@ -12,8 +12,46 @@
         #current-bet Current Bet <span class="black-font">${{this.currentBet}}</span>
         #total-bankroll Your current dollar total in chips is <span class="black-font">${{currentBankRoll}}.</span>
     #game-play-container.full-width.flex-center
-      #users-hand-container.flex-center.half-width Player
-      #dealers-hand-container.flex-center.half-width Dealer
+      #users-hand-container.flex-center.half-width.full-height
+        .user-label.flex-center Player
+        .current-score.flex-center 21
+        .card-container.flex-center
+          .card.flex-center Card
+          .card.flex-center Card
+          .card.flex-center Card
+          .card.flex-center Card
+          .card.flex-center Card
+          .card.flex-center Card
+          .card.flex-center Card
+          .card.flex-center Card
+          .card.flex-center Card
+          .card.flex-center Card
+          .card.flex-center Card
+          .card.flex-center Card
+          .card.flex-center Card
+          .card.flex-center Card
+          .card.flex-center Card
+          .card.flex-center Card
+      #dealers-hand-container.flex-center.half-width.full-height
+        .dealer-label.flex-center Dealer
+        .current-score.flex-center 21
+        .card-container.flex-center
+          .card.flex-center Card
+          .card.flex-center Card
+          .card.flex-center Card
+          .card.flex-center Card
+          .card.flex-center Card
+          .card.flex-center Card
+          .card.flex-center Card
+          .card.flex-center Card
+          .card.flex-center Card
+          .card.flex-center Card
+          .card.flex-center Card
+          .card.flex-center Card
+          .card.flex-center Card
+          .card.flex-center Card
+          .card.flex-center Card
+          .card.flex-center Card
 </template>
 
 <script>
@@ -50,6 +88,10 @@ export default {
   width: 50%;
 }
 
+.full-height {
+  height: 100%;
+}
+
 .black-font {
   color: black;
   font-weight: bold
@@ -64,18 +106,40 @@ export default {
     justify-content: space-around;
     flex-basis: 10%;
     background-color: #9D7E68;
-
-    #game-message {
-      display: flex;
-      justify-content: space-around;
-      flex-direction: column;
-      height: 100%;
-    }
   }
 
   #game-play-container {
     justify-content: space-between;
     flex-basis: 90%;
+
+    #users-hand-container,
+    #dealers-hand-container {
+      flex-direction: column;
+      border-right: 1px solid black;
+
+      .user-label,
+      .dealer-label,
+      .current-score {
+        margin-top: 5px;
+      }
+
+      .card-container {
+        display: flex;
+        justify-content: space-between;
+        padding: 0 10px;
+        flex-wrap: wrap;
+        flex-grow: 1;
+
+
+        .card {
+          height:  20%;
+          flex-basis: 20%;
+          border: 1px solid white;
+          background: white;
+          color: black;
+        }
+      }
+    }
   }
 }
 </style>
